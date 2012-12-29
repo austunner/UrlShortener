2196ef165af751878cf68ae4b9523f2d0c13ba40
austunner <whyhelloaustin@gmail.com>
2012-12-29 00:50:27 -0800

    Clean up

--- src/java/com/urlshortener/hibernate/pojo/User.java
+++ src/java/com/urlshortener/hibernate/pojo/User.java
@@ -4,18 +4,13 @@
  */
 package com.urlshortener.hibernate.pojo;
 
-import java.util.Date;
 import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 /**
  *
